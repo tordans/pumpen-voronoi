b81df9c26c94410a670f075f3029e02541751353
Tobias Jordans <t@tobiasjordans.de>
2020-09-10 22:01:23 +0200

    Hide bike parking data
    The data is broken; did not figure out whats going on. Hidden for now. Maybe a pagination issue?

--- download.sh
+++ download.sh
@@ -2,5 +2,5 @@ http --form POST overpass-api.de/api/interpreter data="[out:json];area[name="Ber
 http --form POST overpass-api.de/api/interpreter data="[out:json];area[name="Berlin"];(way["playground"="swing"](area);node["playground"="swing"](area););out center;" > data--playground--swing.json
 http --form POST overpass-api.de/api/interpreter data="[out:json];area[name="Berlin"];(way["leisure"="playground"](area);node["leisure"="playground"](area););out center;" > data--leisure--playground.json
 http --form POST overpass-api.de/api/interpreter data="[out:json];area[name="Berlin"];(way["advertising"="column"](area);node["advertising"="column"](area););out center;" > data--advertising--column.json
-http --form POST overpass-api.de/api/interpreter data="[out:json];area[name="Berlin"];(way["bicycle_parking"](area);node["bicycle_parking"](area););out center;" > data--bicycle_parking.json
+# http --form POST overpass-api.de/api/interpreter data="[out:json];area[name="Berlin"];(way["bicycle_parking"](area);node["bicycle_parking"](area););out center;" > data--bicycle_parking.json
 http --form POST overpass-api.de/api/interpreter data="[out:json];area[name="Berlin"];(node["highway"="speed_camera"](area);way["highway"="speed_camera"](area););out center;" > data--speed_camera.json
